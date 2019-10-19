b419ffbe013800b360e67dee9ac15e01085f5bb1
yao525896535 <525896535@qq.com>
2019-10-19 18:58:47 +0800

    aaa

--- Test1.java
+++ Test1.java
@@ -3,11 +3,6 @@ public class Test1 {
         System.out.println("class loding");
     }
     public static void main(String [] args) {
-        /*Integer a=128;
-        Integer b=127;
-        Integer a1=128;
-        Integer b1=127;
-        System.out.println(a==a1);
-        System.out.println(b.equals(b1));*/
+
     }
 }
